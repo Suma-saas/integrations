6141a8daf8a36086051d5e68ecfb573c26a07aea
Jhonatan S. Garzon Mape <jgarzonm@nubox.com>
2019-09-02 10:54:03 -0500

    Control errores

--- csvToJson.py
+++ csvToJson.py
@@ -5,30 +5,40 @@ urlApi = 'http://192.168.57.123:8080/'
 data = {}
 textoJson = ""
 
-#Lee el archivo
-with open(r'c:\temp\\example.csv','r') as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for csvRow in csvReader:     	   
-    	a = csvRow['id']	    
-    	data[a]  = csvRow
 
-#Escribir archivo json
-with open(r'c:\temp\\examplejson.json','w') as jsonFile:
-	textoJson = json.dumps(data) 	
-	jsonFile.write(textoJson)	
+try:
+	#Lee el archivo
+	with open(r'c:\temp\\example.csv','r') as csvFile:
+	    csvReader = csv.DictReader(csvFile)
+	    for csvRow in csvReader:     	   
+	    	a = csvRow['id']	    
+	    	data[a]  = csvRow
 
-
-
-with open(r'\\192.168.57.38\temp\Ejemplojson.json','w') as jsonFileErwin:
-	textoJsonErwin = json.dumps(data)	
-	jsonFileErwin.write(textoJsonErwin)	
+except Exception as e:
+	print("Error a el leer el archivo")
 
 
+try:	
+	#Escribir archivo json
+	with open(r'c:\temp\\examplejson.json','w') as jsonFile:
+		textoJson = json.dumps(data) 	
+		jsonFile.write(textoJson)	
+except Exception as e:
+	print("Error escribir json")
+
+try:	
+	with open(r'\\192.168.57.38\temp\Ejemplojson.json','w') as jsonFileErwin:
+		textoJsonErwin = json.dumps(data)	
+		jsonFileErwin.write(textoJsonErwin)	
+
+except Exception as e:
+	print("Error escribir el archivo json en el servidor 192.168.57.38")
 
 
-responseApi = requests.post(urlApi, data=textoJson, headers={'content-type': 'application/json'})
-print("Respuesta " + responseApi.text)
 
-
-
+try:
+	responseApi = requests.post(urlApi, data=textoJson, headers={'content-type': 'application/json'})
+	print("Respuesta " + responseApi.text)
+except Exception as e:
+	print("Error a el enviar la informacion a el Api Url: " + urlApi)
 
